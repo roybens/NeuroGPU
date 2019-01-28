1cec3b4ec914fb7a51db9f5745c9d45bada98142
roybens <bens.roy@gmail.com>
2019-01-28 12:00:22 -0800

    Added test_output

---
+++ Code/test_output.py
@@ -0,0 +1,19 @@
+import array
+params_fn = './AllParams.csv'
+timestep = 3168
+with file(params_fn, 'r') as original:
+    data = original.read()
+splitted = data.split('\n')
+psize = int(splitted[0][:-1])
+psize = 3000
+N = timestep*psize
+
+a = array.array('d')
+with file('./VHotP.dat', 'rb') as f:
+    a.fromfile(f, N)
+maxa = max(a)
+mina = min(a)
+if(maxa<1000 and mina>-1000):
+    print "pass results are good"
+else:
+    print "error:: results are out of physiological bounds"
