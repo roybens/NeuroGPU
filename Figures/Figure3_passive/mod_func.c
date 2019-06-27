b634ffdf0c4f0d5ed33b78d71169500069a471a0
roybens <bens.roy@gmail.com>
2019-06-27 16:34:59 -0700

    passive model working

---
+++ Figures/Figure3_passive/mod_func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "hocdec.h"
+#define IMPORT extern __declspec(dllimport)
+IMPORT int nrnmpi_myid, nrn_nobanner_;
+
+extern void _branching_reg();
+
+void modl_reg(){
+	//nrn_mswindll_stdio(stdin, stdout, stderr);
+    if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+	fprintf(stderr, "Additional mechanisms from files\n");
+
+fprintf(stderr," branching.mod");
+fprintf(stderr, "\n");
+    }
+_branching_reg();
+}
