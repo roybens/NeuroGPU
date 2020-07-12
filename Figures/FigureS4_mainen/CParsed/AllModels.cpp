3cacacf0693c8798cb53a02a38b256390ca67a1f
roybens <bens.roy@gmail.com>
2020-07-11 23:58:24 -0700

    Figure S4Mainen Working

---
+++ Figures/FigureS4_mainen/CParsed/AllModels.cpp
@@ -0,0 +1,293 @@
+// Automatically generated C for E:\GitHub\NeuroGPU\Figures\FigureS4_mainen./runModel.hoc
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "AllModels.h"
+
+// Universals:
+#define PI (3.1415927f)
+#define R (8.31441f)
+#define FARADAY (96485.309f)
+#define ktf (1000.*8.3134*(celsius + 273.15)/FARADAY)
+
+// Reversals:
+#define ena (60.0f)
+#define ek (-90.0f)
+#define DEF_cai		5.e-5	   /* mM */
+#define DEF_cao		2.	   /* mM */
+
+// Locals:
+MYFTYPE a,b,drive_channel,gca,gk,gna,hinf,htau,minf,mtau,ninf,ntau,vi;
+
+// Ion currents as Locals:
+MYFTYPE ik,ica,ina;
+
+// NGlobals:
+#define q10_ca (2.3)
+#define temp_ca (23.0)
+#define tadj_ca (3.2093639532679714)
+#define vmin_ca (-120.0)
+#define vmax_ca (100.0)
+#define vshift_ca (0.0)
+#define depth_cad (0.09334562733124982)
+#define cainf_cad (0.0001)
+#define taur_cad (200.0)
+#define q10_kca (2.3)
+#define temp_kca (23.0)
+#define tadj_kca (3.2093639532679714)
+#define vmin_kca (-120.0)
+#define vmax_kca (100.0)
+#define q10_km (2.3)
+#define temp_km (23.0)
+#define tadj_km (3.2093639532679714)
+#define vmin_km (-120.0)
+#define vmax_km (100.0)
+#define q10_kv (2.3)
+#define temp_kv (23.0)
+#define tadj_kv (3.2093639532679714)
+#define vmin_kv (-120.0)
+#define vmax_kv (100.0)
+#define q10_na (2.3)
+#define temp_na (23.0)
+#define tadj_na (3.2093639532679714)
+#define vmin_na (-120.0)
+#define vmax_na (100.0)
+#define vshift_na (-5.0)
+
+// Declarations:
+void trates_ca(MYFTYPE v ,MYFTYPE gbar_ca,MYFTYPE cao_ca);
+void rates_ca(MYFTYPE vm ,MYFTYPE gbar_ca,MYFTYPE cao_ca);
+void rates_kca(MYFTYPE cai,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca);
+void trates_km(MYFTYPE v ,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km);
+void rates_km(MYFTYPE v ,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km);
+void trates_kv(MYFTYPE v ,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv);
+void rates_kv(MYFTYPE v ,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv);
+void trates_na(MYFTYPE v,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na);
+void rates_na(MYFTYPE vm,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na);
+
+// Functions:
+MYFTYPE efun_ca(MYFTYPE z){
+	if (fabs(z) < 1e-4) {;
+		return  1 - z/2;
+	}else{;
+		return  z/(exp(z) - 1);
+	};
+};
+MYFTYPE efun_km(MYFTYPE z){
+	if (fabs(z) < 1e-4) {;
+		return  1 - z/2;
+	}else{;
+		return  z/(exp(z) - 1);
+	};
+};
+MYFTYPE efun_kv(MYFTYPE z){
+	if (fabs(z) < 1e-4) {;
+		return  1 - z/2;
+	}else{;
+		return  z/(exp(z) - 1);
+	};
+};
+MYFTYPE trap0_na(MYFTYPE v,MYFTYPE th,MYFTYPE a,MYFTYPE q){
+	if (fabs((v-th)/q) > 1e-6) {;
+	        return  a * (v - th) / (1 - exp(-(v - th)/q));
+	} else {;
+	        return  a * q;
+	};
+}	;
+
+// Inits:
+
+
+void InitModel_ca(MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_ca,MYFTYPE cao_ca, MYFTYPE cai, MYFTYPE &ica,MYFTYPE &eca){
+eca = ktf/2 *log(DEF_cao / cai);
+eca = ktf/2 *log(DEF_cao / cai);
+   /* removed tadj_ca recalculation */
+	trates_ca(v+vshift_ca,gbar_ca,cao_ca);
+	m = minf;
+	h = hinf;
+};
+
+
+void InitModel_cad(MYFTYPE v,MYFTYPE &ca, MYFTYPE ica, MYFTYPE &cai,MYFTYPE &eca){
+cai = DEF_cai;
+eca = ktf/2 *log(DEF_cao / cai);
+	ca = cainf_cad;
+	cai = ca;
+};
+
+
+void InitModel_kca(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca, MYFTYPE cai,MYFTYPE &eca){
+eca = ktf/2 *log(DEF_cao / cai);
+	rates_kca(cai,gbar_kca,caix_kca,Ra_kca,Rb_kca);
+	n = ninf;
+};
+
+
+void InitModel_km(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km){
+   /* removed tadj_km recalculation */
+	trates_km(v,gbar_km,tha_km,qa_km,Ra_km,Rb_km);
+	n = ninf;
+};
+
+
+void InitModel_kv(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv){
+   /* removed tadj_kv recalculation */
+	trates_kv(v,gbar_kv,tha_kv,qa_kv,Ra_kv,Rb_kv);
+	n = ninf;
+};
+
+
+void InitModel_na(MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na){
+   /* removed tadj_na recalculation */
+	trates_na(v+vshift_na,gbar_na,tha_na,qa_na,Ra_na,Rb_na,thi1_na,thi2_na,qi_na,thinf_na,qinf_na,Rg_na,Rd_na);
+	m = minf;
+	h = hinf;
+};
+
+
+void InitModel_pas(MYFTYPE v,MYFTYPE g_pas,MYFTYPE e_pas){
+};
+
+
+void InitModel_pas2(MYFTYPE v,MYFTYPE g_pas2,MYFTYPE e_pas2){
+};
+
+
+void InitModel_pasx(MYFTYPE v,MYFTYPE g_pasx,MYFTYPE e_pasx){
+};
+
+// Procedures:
+void trates_ca(MYFTYPE v ,MYFTYPE gbar_ca,MYFTYPE cao_ca) {
+   rates_ca (   v ,gbar_ca,cao_ca) ;
+}
+void rates_ca(MYFTYPE vm ,MYFTYPE gbar_ca,MYFTYPE cao_ca) {
+   MYFTYPE a , b ;
+/* removed tadj_ca recalculation */
+   a = 0.209 * efun_ca (   - ( 27.0 + vm ) / 3.8 ) ;
+   b = 0.94 * exp ( ( - 75.0 - vm ) / 17.0 ) ;
+   mtau = 1.0 / tadj_ca / ( a + b ) ;
+   minf = a / ( a + b ) ;
+   a = 0.000457 * exp ( ( - 13.0 - vm ) / 50.0 ) ;
+   b = 0.0065 / ( exp ( ( - vm - 15.0 ) / 28.0 ) + 1.0 ) ;
+   htau = 1.0 / tadj_ca / ( a + b ) ;
+   hinf = a / ( a + b ) ;
+}
+void rates_kca(MYFTYPE cai,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca) {
+   a = Ra_kca * powf( cai , caix_kca ) ;
+   b = Rb_kca ;
+  /* removed tadj_kca recalculation */
+   ntau = 1.0 / tadj_kca / ( a + b ) ;
+   ninf = a / ( a + b ) ;
+}
+void trates_km(MYFTYPE v ,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km) {
+   rates_km (   v ,gbar_km,tha_km,qa_km,Ra_km,Rb_km) ;
+}
+void rates_km(MYFTYPE v ,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km) {
+   a = Ra_km * qa_km * efun_km (   - ( v - tha_km ) / qa_km ) ;
+   b = Rb_km * qa_km * efun_km (   ( v - tha_km ) / qa_km ) ;
+  /* removed tadj_km recalculation */
+   ntau = 1.0 / tadj_km / ( a + b ) ;
+   ninf = a / ( a + b ) ;
+}
+void trates_kv(MYFTYPE v ,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv) {
+   rates_kv (   v ,gbar_kv,tha_kv,qa_kv,Ra_kv,Rb_kv) ;
+}
+void rates_kv(MYFTYPE v ,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv) {
+   a = Ra_kv * qa_kv * efun_kv (   - ( v - tha_kv ) / qa_kv ) ;
+   b = Rb_kv * qa_kv * efun_kv (   ( v - tha_kv ) / qa_kv ) ;
+  /* removed tadj_kv recalculation */
+   ntau = 1.0 / tadj_kv / ( a + b ) ;
+   ninf = a / ( a + b ) ;
+}
+void trates_na(MYFTYPE v,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na) {
+   rates_na (   v ,gbar_na,tha_na,qa_na,Ra_na,Rb_na,thi1_na,thi2_na,qi_na,thinf_na,qinf_na,Rg_na,Rd_na) ;
+}
+void rates_na(MYFTYPE vm,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na) {
+   MYFTYPE a , b ;
+ a = trap0_na (   vm , tha_na , Ra_na , qa_na ) ;
+   b = trap0_na (   - vm , - tha_na , Rb_na , qa_na ) ;
+  /* removed tadj_na recalculation */
+   mtau = 1.0 / tadj_na / ( a + b ) ;
+   minf = a / ( a + b ) ;
+   a = trap0_na (   vm , thi1_na , Rd_na , qi_na ) ;
+   b = trap0_na (   - vm , - thi2_na , Rg_na , qi_na ) ;
+   htau = 1.0 / tadj_na / ( a + b ) ;
+   hinf = 1.0 / ( 1.0 + exp ( ( vm - thinf_na ) / qinf_na ) ) ;
+}
+
+// Derivs:
+
+// Breaks:
+
+
+void BreakpointModel_ca(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_ca,MYFTYPE cao_ca, MYFTYPE cai, MYFTYPE &ica,MYFTYPE &eca) {
+MYFTYPE ica_ca;
+
+   gca = tadj_ca * gbar_ca * m * m * h ;
+   ica_ca = ( 1e-4 ) * gca * ( v - eca ) ;
+sumCurrents+= ica_ca;
+
+ ica += ica_ca;
+sumConductivity+= gca;
+};
+
+
+void BreakpointModel_cad(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &ca, MYFTYPE ica, MYFTYPE &cai,MYFTYPE &eca) {
+};
+
+
+void BreakpointModel_kca(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca, MYFTYPE cai,MYFTYPE &eca) {
+   gk = tadj_kca * gbar_kca * n ;
+   ik = ( 1e-4 ) * gk * ( v - ek ) ;
+sumCurrents+= ik;
+sumConductivity+= gk;
+};
+
+
+void BreakpointModel_km(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km) {
+   gk = tadj_km * gbar_km * n ;
+   ik = ( 1e-4 ) * gk * ( v - ek ) ;
+sumCurrents+= ik;
+sumConductivity+= gk;
+};
+
+
+void BreakpointModel_kv(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv) {
+   gk = tadj_kv * gbar_kv * n ;
+   ik = ( 1e-4 ) * gk * ( v - ek ) ;
+sumCurrents+= ik;
+sumConductivity+= gk;
+};
+
+
+void BreakpointModel_na(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na) {
+   gna = tadj_na * gbar_na * m * m * m * h ;
+   ina = ( 1e-4 ) * gna * ( v - ena ) ;
+sumCurrents+= ina;
+sumConductivity+= gna;
+};
+
+
+void BreakpointModel_pas(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pas,MYFTYPE e_pas) {
+   i = g_pas * ( v - e_pas ) ;
+i = i;
+
+sumCurrents+= i;
+sumConductivity+= g_pas;
+};
+
+
+void BreakpointModel_pas2(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pas2,MYFTYPE e_pas2) {
+   i = g_pas2 * ( v - e_pas2 ) ;
+i = i;
+
+sumCurrents+= i;
+};
+
+
+void BreakpointModel_pasx(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pasx,MYFTYPE e_pasx) {
+   i = g_pasx * ( v - e_pasx ) ;
+i = i;
+
+sumCurrents+= i;
+};
---
+++ Figures/FigureS4_mainen/CParsed/AllModels.cuh
@@ -0,0 +1,72 @@
+// Automatically generated CUH for E:\GitHub\NeuroGPU\Figures\FigureS4_mainen./runModel.hoc
+
+#ifndef __ALLMODELSCU__
+#define __ALLMODELSCU__
+#include "Util.h"
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#define NSEG 384
+#define NCOMPS 177
+#define LOG_N_DEPTH 6
+#define N_MODELS 7
+#define N_FATHERS 99
+#define N_CALL_FOR_FATHER 186
+#define COMP_DEPTH 26
+#define N_L_REL 24
+#define N_F_L_REL 23
+#define NILP 12
+#define NSTATES 8
+#define NPARAMS 34
+
+#define NTRACES 1
+
+__device__ void CuInitModel_ca(MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_ca,MYFTYPE cao_ca, MYFTYPE cai, MYFTYPE &ica,MYFTYPE &eca);
+__device__ void CuInitModel_cad(MYFTYPE v,MYFTYPE &ca, MYFTYPE ica, MYFTYPE &cai,MYFTYPE &eca);
+__device__ void CuInitModel_kca(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca, MYFTYPE cai,MYFTYPE &eca);
+__device__ void CuInitModel_km(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km);
+__device__ void CuInitModel_kv(MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv);
+__device__ void CuInitModel_na(MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na);
+__device__ void CuInitModel_pas(MYFTYPE v,MYFTYPE g_pas,MYFTYPE e_pas);
+__device__ void CuInitModel_pas2(MYFTYPE v,MYFTYPE g_pas2,MYFTYPE e_pas2);
+__device__ void CuInitModel_pasx(MYFTYPE v,MYFTYPE g_pasx,MYFTYPE e_pasx);
+__device__ void CuDerivModel_ca(MYFTYPE dt, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_ca,MYFTYPE cao_ca, MYFTYPE cai, MYFTYPE &ica,MYFTYPE &eca);
+__device__ void CuDerivModel_cad(MYFTYPE dt, MYFTYPE v,MYFTYPE &ca, MYFTYPE ica, MYFTYPE &cai,MYFTYPE &eca);
+__device__ void CuDerivModel_kca(MYFTYPE dt, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca, MYFTYPE cai,MYFTYPE &eca);
+__device__ void CuDerivModel_km(MYFTYPE dt, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km);
+__device__ void CuDerivModel_kv(MYFTYPE dt, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv);
+__device__ void CuDerivModel_na(MYFTYPE dt, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na);
+__device__ void CuBreakpointModel_ca(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_ca,MYFTYPE cao_ca, MYFTYPE cai, MYFTYPE &ica,MYFTYPE &eca) ;
+
+__device__ void CuBreakpointModel_cad(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &ca, MYFTYPE ica, MYFTYPE &cai,MYFTYPE &eca) ;
+
+__device__ void CuBreakpointModel_kca(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kca,MYFTYPE caix_kca,MYFTYPE Ra_kca,MYFTYPE Rb_kca, MYFTYPE cai,MYFTYPE &eca) ;
+
+__device__ void CuBreakpointModel_km(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_km,MYFTYPE tha_km,MYFTYPE qa_km,MYFTYPE Ra_km,MYFTYPE Rb_km) ;
+
+__device__ void CuBreakpointModel_kv(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &n,MYFTYPE gbar_kv,MYFTYPE tha_kv,MYFTYPE qa_kv,MYFTYPE Ra_kv,MYFTYPE Rb_kv) ;
+
+__device__ void CuBreakpointModel_na(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE &m,MYFTYPE &h,MYFTYPE gbar_na,MYFTYPE tha_na,MYFTYPE qa_na,MYFTYPE Ra_na,MYFTYPE Rb_na,MYFTYPE thi1_na,MYFTYPE thi2_na,MYFTYPE qi_na,MYFTYPE thinf_na,MYFTYPE qinf_na,MYFTYPE Rg_na,MYFTYPE Rd_na) ;
+
+__device__ void CuBreakpointModel_pas(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pas,MYFTYPE e_pas) ;
+
+__device__ void CuBreakpointModel_pas2(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pas2,MYFTYPE e_pas2) ;
+
+__device__ void CuBreakpointModel_pasx(MYSECONDFTYPE &sumCurrents, MYFTYPE &sumConductivity, MYFTYPE v,MYFTYPE g_pasx,MYFTYPE e_pasx) ;
+
+
+
+#define SET_STATES(VARILP) ModelStates_## VARILP [0]=0; ModelStates_## VARILP [1]=0; ModelStates_## VARILP [2]=0; ModelStates_## VARILP [3]=0; ModelStates_## VARILP [4]=0; ModelStates_## VARILP [5]=0; ModelStates_## VARILP [6]=0; ModelStates_## VARILP [7]=0; ; 
+
+
+#define CALL_TO_INIT_STATES_CU(VARILP) if(cBoolModel[PIdx[count] +0*NSEG]){CuInitModel_ca(v[count],state_macro(0,count) ,state_macro(1,count) ,param_macro(0, PIdx[count]) ,param_macro(1, PIdx[count]) , cai[count] , ica[count] ,eca[count] );}if(cBoolModel[PIdx[count] +1*NSEG]){CuInitModel_cad(v[count],state_macro(2,count) , ica[count] , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +2*NSEG]){CuInitModel_kca(v[count],state_macro(3,count) ,param_macro(2, PIdx[count]) ,param_macro(3, PIdx[count]) ,param_macro(4, PIdx[count]) ,param_macro(5, PIdx[count]) , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +3*NSEG]){CuInitModel_km(v[count],state_macro(4,count) ,param_macro(6, PIdx[count]) ,param_macro(7, PIdx[count]) ,param_macro(8, PIdx[count]) ,param_macro(9, PIdx[count]) ,param_macro(10, PIdx[count]) );}if(cBoolModel[PIdx[count] +4*NSEG]){CuInitModel_kv(v[count],state_macro(5,count) ,param_macro(11, PIdx[count]) ,param_macro(12, PIdx[count]) ,param_macro(13, PIdx[count]) ,param_macro(14, PIdx[count]) ,param_macro(15, PIdx[count]) );}if(cBoolModel[PIdx[count] +5*NSEG]){CuInitModel_na(v[count],state_macro(6,count) ,state_macro(7,count) ,param_macro(16, PIdx[count]) ,param_macro(17, PIdx[count]) ,param_macro(18, PIdx[count]) ,param_macro(19, PIdx[count]) ,param_macro(20, PIdx[count]) ,param_macro(21, PIdx[count]) ,param_macro(22, PIdx[count]) ,param_macro(23, PIdx[count]) ,param_macro(24, PIdx[count]) ,param_macro(25, PIdx[count]) ,param_macro(26, PIdx[count]) ,param_macro(27, PIdx[count]) );}if(cBoolModel[PIdx[count] +6*NSEG]){CuInitModel_pas(v[count],param_macro(28, PIdx[count]) ,param_macro(29, PIdx[count]) );}if(cBoolModel[PIdx[count] +7*NSEG]){CuInitModel_pas2(v[count],param_macro(30, PIdx[count]) ,param_macro(31, PIdx[count]) );}if(cBoolModel[PIdx[count] +8*NSEG]){CuInitModel_pasx(v[count],param_macro(32, PIdx[count]) ,param_macro(33, PIdx[count]) );}
+
+#define CALL_TO_DERIV_CU(VARILP)   if(cBoolModel[PIdx[count] +0*NSEG]){CuDerivModel_ca(dt, v[count],state_macro(0,count) ,state_macro(1,count) ,param_macro(0, PIdx[count]) ,param_macro(1, PIdx[count]) , cai[count] , ica[count] ,eca[count] );}if(cBoolModel[PIdx[count] +1*NSEG]){CuDerivModel_cad(dt, v[count],state_macro(2,count) , ica[count] , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +2*NSEG]){CuDerivModel_kca(dt, v[count],state_macro(3,count) ,param_macro(2, PIdx[count]) ,param_macro(3, PIdx[count]) ,param_macro(4, PIdx[count]) ,param_macro(5, PIdx[count]) , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +3*NSEG]){CuDerivModel_km(dt, v[count],state_macro(4,count) ,param_macro(6, PIdx[count]) ,param_macro(7, PIdx[count]) ,param_macro(8, PIdx[count]) ,param_macro(9, PIdx[count]) ,param_macro(10, PIdx[count]) );}if(cBoolModel[PIdx[count] +4*NSEG]){CuDerivModel_kv(dt, v[count],state_macro(5,count) ,param_macro(11, PIdx[count]) ,param_macro(12, PIdx[count]) ,param_macro(13, PIdx[count]) ,param_macro(14, PIdx[count]) ,param_macro(15, PIdx[count]) );}if(cBoolModel[PIdx[count] +5*NSEG]){CuDerivModel_na(dt, v[count],state_macro(6,count) ,state_macro(7,count) ,param_macro(16, PIdx[count]) ,param_macro(17, PIdx[count]) ,param_macro(18, PIdx[count]) ,param_macro(19, PIdx[count]) ,param_macro(20, PIdx[count]) ,param_macro(21, PIdx[count]) ,param_macro(22, PIdx[count]) ,param_macro(23, PIdx[count]) ,param_macro(24, PIdx[count]) ,param_macro(25, PIdx[count]) ,param_macro(26, PIdx[count]) ,param_macro(27, PIdx[count]) );}if(cBoolModel[PIdx[count] +6*NSEG]){}if(cBoolModel[PIdx[count] +7*NSEG]){}if(cBoolModel[PIdx[count] +8*NSEG]){}
+
+#define CALL_TO_BREAK_CU(VARILP)   if(cBoolModel[PIdx[count] +0*NSEG]){CuBreakpointModel_ca(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(0,count) ,state_macro(1,count) ,param_macro(0, PIdx[count]) ,param_macro(1, PIdx[count]) , cai[count] , ica[count] ,eca[count] );}if(cBoolModel[PIdx[count] +1*NSEG]){CuBreakpointModel_cad(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(2,count) , ica[count] , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +2*NSEG]){CuBreakpointModel_kca(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(3,count) ,param_macro(2, PIdx[count]) ,param_macro(3, PIdx[count]) ,param_macro(4, PIdx[count]) ,param_macro(5, PIdx[count]) , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +3*NSEG]){CuBreakpointModel_km(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(4,count) ,param_macro(6, PIdx[count]) ,param_macro(7, PIdx[count]) ,param_macro(8, PIdx[count]) ,param_macro(9, PIdx[count]) ,param_macro(10, PIdx[count]) );}if(cBoolModel[PIdx[count] +4*NSEG]){CuBreakpointModel_kv(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(5,count) ,param_macro(11, PIdx[count]) ,param_macro(12, PIdx[count]) ,param_macro(13, PIdx[count]) ,param_macro(14, PIdx[count]) ,param_macro(15, PIdx[count]) );}if(cBoolModel[PIdx[count] +5*NSEG]){CuBreakpointModel_na(sumCurrents[count] , sumConductivity[count],v[count] ,state_macro(6,count) ,state_macro(7,count) ,param_macro(16, PIdx[count]) ,param_macro(17, PIdx[count]) ,param_macro(18, PIdx[count]) ,param_macro(19, PIdx[count]) ,param_macro(20, PIdx[count]) ,param_macro(21, PIdx[count]) ,param_macro(22, PIdx[count]) ,param_macro(23, PIdx[count]) ,param_macro(24, PIdx[count]) ,param_macro(25, PIdx[count]) ,param_macro(26, PIdx[count]) ,param_macro(27, PIdx[count]) );}if(cBoolModel[PIdx[count] +6*NSEG]){CuBreakpointModel_pas(sumCurrents[count] , sumConductivity[count],v[count] ,param_macro(28, PIdx[count]) ,param_macro(29, PIdx[count]) );}if(cBoolModel[PIdx[count] +7*NSEG]){CuBreakpointModel_pas2(sumCurrents[count] , sumConductivity[count],v[count] ,param_macro(30, PIdx[count]) ,param_macro(31, PIdx[count]) );}if(cBoolModel[PIdx[count] +8*NSEG]){CuBreakpointModel_pasx(sumCurrents[count] , sumConductivity[count],v[count] ,param_macro(32, PIdx[count]) ,param_macro(33, PIdx[count]) );}
+
+#define CALL_TO_BREAK_DV_CU(VARILP)    if(cBoolModel[PIdx[count] +0*NSEG]){CuBreakpointModel_ca(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(0,count) ,state_macro(1,count) ,param_macro(0, PIdx[count]) ,param_macro(1, PIdx[count]) , cai[count] , temp,eca[count] );}if(cBoolModel[PIdx[count] +1*NSEG]){CuBreakpointModel_cad(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(2,count) , temp, cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +2*NSEG]){CuBreakpointModel_kca(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(3,count) ,param_macro(2, PIdx[count]) ,param_macro(3, PIdx[count]) ,param_macro(4, PIdx[count]) ,param_macro(5, PIdx[count]) , cai[count] ,eca[count] );}if(cBoolModel[PIdx[count] +3*NSEG]){CuBreakpointModel_km(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(4,count) ,param_macro(6, PIdx[count]) ,param_macro(7, PIdx[count]) ,param_macro(8, PIdx[count]) ,param_macro(9, PIdx[count]) ,param_macro(10, PIdx[count]) );}if(cBoolModel[PIdx[count] +4*NSEG]){CuBreakpointModel_kv(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(5,count) ,param_macro(11, PIdx[count]) ,param_macro(12, PIdx[count]) ,param_macro(13, PIdx[count]) ,param_macro(14, PIdx[count]) ,param_macro(15, PIdx[count]) );}if(cBoolModel[PIdx[count] +5*NSEG]){CuBreakpointModel_na(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,state_macro(6,count) ,state_macro(7,count) ,param_macro(16, PIdx[count]) ,param_macro(17, PIdx[count]) ,param_macro(18, PIdx[count]) ,param_macro(19, PIdx[count]) ,param_macro(20, PIdx[count]) ,param_macro(21, PIdx[count]) ,param_macro(22, PIdx[count]) ,param_macro(23, PIdx[count]) ,param_macro(24, PIdx[count]) ,param_macro(25, PIdx[count]) ,param_macro(26, PIdx[count]) ,param_macro(27, PIdx[count]) );}if(cBoolModel[PIdx[count] +6*NSEG]){CuBreakpointModel_pas(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,param_macro(28, PIdx[count]) ,param_macro(29, PIdx[count]) );}if(cBoolModel[PIdx[count] +7*NSEG]){CuBreakpointModel_pas2(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,param_macro(30, PIdx[count]) ,param_macro(31, PIdx[count]) );}if(cBoolModel[PIdx[count] +8*NSEG]){CuBreakpointModel_pasx(sumCurrentsDv[count] , sumConductivityDv[count] ,v[count] +0.001,param_macro(32, PIdx[count]) ,param_macro(33, PIdx[count]) );}
+
+
+#endif
