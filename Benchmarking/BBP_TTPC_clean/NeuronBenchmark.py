d871f5ddb8c782a58b9fd32ac39c278d6948c0d5
roybens <bens.roy@gmail.com>
2020-10-06 16:26:18 -0700

    updating benchmark

--- Benchmarking/BBP_TTPC_clean/NeuronBenchmark.py
+++ Benchmarking/BBP_TTPC_clean/NeuronBenchmark.py
@@ -8,33 +8,54 @@ Created on Mon Oct  5 13:44:17 2020
 import sys
 from neuron import h, gui
 import numpy as np
+import matplotlib.pyplot as plt
 
 h.load_file("runModel.hoc")
 param_list = np.loadtxt('./params/params.csv')
+
 pc = h.ParallelContext()
-h.dt = 0.001
+h.dt = 0.1
 ntimesteps = 5000
 tstop = ntimesteps*h.dt
+pc.set_maxstep(10)
+root_name = h.secname(sec=h.cell.soma[0])
 class Cell():
   def __init__(self, gid):
-    self.soma = h.Section(name="soma", cell=self)
+    print(root_name)
     self.gid = gid
     pc.set_gid2node(gid, pc.id())
-    h.transvec = h.Vector(param_list[gid])
+    curr_p = param_list[gid]*gid
+    h.transvec = h.Vector(curr_p)
     h.tfunc()
+    #ic = h.IClamp(h.cell.soma[0](.5))
+    h.st.delay = 100
+    h.st.dur = 100
+    h.st.amp = 0.5
+    self.v = h.Vector()
+    self.v.record(h.cell.soma[0](.5)._ref_v,sec = h.cell.soma[0])
+
 
 ncell = len(param_list)
+ncell = 100
 gids = range(pc.id(), ncell) # round robin
 cells = [Cell(gid) for gid in gids]
 
 # make sure to enable cache efficiency
 h.cvode.cache_efficient(1)
+#print(pc.dt())
 def prun():
-    h.finitialize(h.v_init)
+    
+    h.finitialize(-65)
     h.tstop = tstop
     pc.psolve(tstop)
 prun()
-    
+allvs = []
+for curr_cell in cells:
+    curr_vs = curr_cell.v.to_python()
+    allvs.append(curr_vs)
+    plt.plot(curr_vs)
+vsstd = np.mean(allvs,0)
+max(vsstd)
 # #myobj = h.NetCon(h.soma(0.5)._ref_v, None, sec=h.soma)
 # #pc.cell(pc.id()+1, myobj)
 
